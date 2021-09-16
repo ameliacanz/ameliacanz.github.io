02f2fc991b31b10fff59c8d6a7f270821acfb99c
hadi-api <87928412+hadi-api@users.noreply.github.com>
2021-09-16 10:32:28 +0700

    update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -180,8 +180,8 @@ function net() {
 	});
 }
 net();
-var music = music||true;
-if(music !==false||music !=="false"){
+var music = music||1;
+if(music == 1){
 	swal.fire({
 		title: 'do you want to play your own song from youtube?',
 		icon: 'warning',
