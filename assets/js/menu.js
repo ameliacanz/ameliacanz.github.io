4dc77920618d55cde50ad61d6b11161c7392d160
hadi-api <87928412+hadi-api@users.noreply.github.com>
2021-09-28 11:56:05 +0700

    update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -16,11 +16,7 @@ try {
 			position: 'top-end',
 			showConfirmButton: false,
 			timer: 5000,
-			timerProgressBar: true,
-			didOpen: (toast) => {
-				toast.addEventListener('mouseenter', Swal.stopTimer)
-				toast.addEventListener('mouseleave', Swal.resumeTimer)
-			}
+			timerProgressBar: true
 		})
 
 		Toast.fire({
