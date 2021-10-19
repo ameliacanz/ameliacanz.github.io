54aec174a0e4f03c3a411dd5371d476efc3ce520
hadi-api <87928412+hadi-api@users.noreply.github.com>
2021-10-19 11:48:48 +0700

    update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -77,11 +77,11 @@ try {
 	}, 10);
 
 	//-- CPU Usage detection
-	fetch("https://hadi-api.herokuapp.com/system/about").then(res=>res.json()).then(res=>{
+	fetch("https://hadi-api.herokuapp.com/system/about").then(res=>res.json()).then(res=> {
 		tag_cpu.innerHTML = `${res.memoryUsage.rss}<small>/ 500MB</small>`;
 	});
 	setInterval(function() {
-		fetch("https://hadi-api.herokuapp.com/system/about").then(res=>res.json()).then(res=>{
+		fetch("https://hadi-api.herokuapp.com/system/about").then(res=>res.json()).then(res=> {
 			tag_cpu.innerHTML = `${res.memoryUsage.rss}<small>/ 500MB</small>`;
 		});
 	}, 2000);
@@ -209,8 +209,8 @@ try {
 	function net() {
 		fetch('https://hadi-api.herokuapp.com/api/ip').then(res=>res.json()).then(res=> {
 			res = res.result;
-			if(!region.includes(res.countryCode)) isVPN = true;
-			tag_netinfo.innerHTML = `<b>IP: </b>${res.ip}<br><b>ISP: </b>${res.isp}<br><b>CITY: </b>${res.city}<br><b>DISTRICT: </b>${res.district}<br><b>TIMEZONE: </b>${res.timezone}<br><b>COUNTRY: </b>${res.country}<br><b>VPN: </b>${region.includes(res.countryCode) ? "false":"true"}<br><b>LATITUDE: </b>${res.latitude}<br><b>LONGITUDE: </b>${res.longitude}`;
+			if (!region.includes(res.countryCode)) isVPN = true;
+			tag_netinfo.innerHTML = `<b>IP: </b>${res.ip}<br><b>ISP: </b>${res.isp}<br><b>CITY: </b>${res.city}<br><b>DISTRICT: </b>${res.district}<br><b>TIMEZONE: </b>${res.timezone}<br><b>COUNTRY: </b>${res.country}<br><b>VPN: </b>${region.includes(res.countryCode) ? "false": "true"}<br><b>LATITUDE: </b>${res.latitude}<br><b>LONGITUDE: </b>${res.longitude}`;
 		});
 	}
 	net();
